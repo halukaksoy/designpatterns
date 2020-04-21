d1c72c816b65f992f5babf6d047c5213a7d58096
halukaksoy <halukaksoy@outlook.com>
2020-04-21 11:44:56 +0300

    Add Prototype

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -6,7 +6,58 @@ namespace Prototype
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            OrderManager orderManager = new OrderManager();
+            var order1 = orderManager.CreateOrder(1, DateTime.Now, OrderType.Sales);
+            var order2 = (Order)order1.Clone();
+            order2.Id = 2;
+            if (order1.Id == order2.Id)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("NotEqual");
+            }
+            Console.ReadLine();
+        }
+    }
+    //Abstract Prototype
+    abstract class OrderPrototype
+    {
+        public abstract OrderPrototype Clone();
+    }
+    enum OrderType
+    {
+        Sales,
+        Purchase
+    }
+    class Order : OrderPrototype
+    {
+        private int _id;
+        private DateTime _orderDate;
+        private OrderType _orderType;
+        public Order(int id, DateTime orderDate, OrderType orderType)
+        {
+            _id = id;
+            _orderDate = orderDate;
+            _orderType = orderType;
+        }
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public override OrderPrototype Clone()
+        {
+            return this.MemberwiseClone() as OrderPrototype;
+        }
+    }
+
+    class OrderManager
+    {
+        public Order CreateOrder(int id, DateTime orderDate, OrderType orderType)
+        {
+            return new Order(id, orderDate, orderType);
         }
     }
 }
