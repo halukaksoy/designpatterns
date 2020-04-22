e110aab0a321d818d7e8b832ccccec1a2e628894
halukaksoy <halukaksoy@outlook.com>
2020-04-22 19:56:50 +0300

    Add Adapter Pattern

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -6,7 +6,36 @@ namespace Adapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TextDocument textDocument = new HtmlAdapter();
+            textDocument.Request();
+
+            Console.ReadLine();
+        }
+    }
+    //Target
+    class TextDocument
+    {
+        public virtual void Request()
+        {
+            Console.WriteLine("Called Text Document Request");
+        }
+    }
+    //Adapter
+    class HtmlAdapter : TextDocument
+    {
+        private HtmlDocument _htmlDocument = new HtmlDocument();
+        public override void Request()
+        {
+            _htmlDocument.ConvertRequest();
+        }
+    }
+
+    //Adaptee
+    class HtmlDocument
+    {
+        public void ConvertRequest()
+        {
+            Console.WriteLine("<html><head></head><body><label>Called Html Convert Request</label></body></html>");
         }
     }
 }
---
+++ Adapter/README.md
@@ -0,0 +1,24 @@
+# Adapter
+
+### Live Demo
+
+[DotNetFiddle](https://dotnetfiddle.net/4s7wS3)
+
+----
+### Definition
+
+Convert the interface of a class into another interface clients expect. 
+Adapter lets classes work together that couldn't otherwise because of incompatible interfaces.
+
+----
+### UML class diagram
+![](https://www.dofactory.com/images/diagrams/net/adapter.gif)
+
+### Frequency of UseMedium high
+![](https://www.dofactory.com/images/use_medium_high.gif) High
+
+----
+### Resources
+
+[DoFactory](https://www.dofactory.com/net/adapter-design-pattern)
+
